ceea80b194d64a35a469bafdaa2a3831cd5011e9
Sohail Somani <Sohail Somani>
2016-10-24 11:33:35 -0400

    Restore typings

--- build.bat
+++ build.bat
@@ -4,14 +4,17 @@ rd /s /q dist
 
 cd module1 && ^
 call npm install && ^
+call typings install && ^
 call gulp && ^
 cd .. && ^
 cd module2 && ^
 call npm install && ^
+call typings install && ^
 call gulp && ^
 cd .. && ^
 cd client && ^
 call npm install && ^
+call typings install && ^
 call bower install && ^
 call gulp && ^
 cd .. && ^
